665c2d4729da2be651872f59adeda3891a96716b
Sujitha <sujitha@railsfactory.org>
2014-07-10 11:09:56 +0530

    current bulk commit - senthil

--- roman/lib/roman.rb
+++ roman/lib/roman.rb
@@ -1,12 +1,26 @@
 class Roman
 
-  def self.convert(number)
-    if number ==1
-    'I'
-  else
-    'II'
+  def self.to_roman
+   
+      {
+      1000 => "M",
+      500 => "D",
+      100 => "C",
+      50 => "L",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+    }
+ end
+def self.convert(number, result = "")
+   return result if number == 0
+  to_roman.keys.each do |divisor|
+    quotient, modulus = number.divmod(divisor)
+    result << to_roman[divisor] * quotient
+    return self.convert(modulus, result) if quotient > 0
   end
-  
-  end
-
+ end 
 end  
+
--- roman/test/roman_test.rb
+++ roman/test/roman_test.rb
@@ -10,10 +10,15 @@ class TestRoman < Test::Unit::TestCase
 
 
   def test_one
-    assert_equal 'I',Roman.convert(1)
-    assert_equal 'II',Roman.convert(2)
-    assert_equal 'III',Roman.convert(3)
+    assert_equal "I",Roman.convert(1)
+    assert_equal "L",Roman.convert(50)
+    assert_equal "X",Roman.convert(10)
     assert_equal 'IV',Roman.convert(4)
+    assert_equal "XI",Roman.convert(11)
+    assert_equal "",Roman.convert(0)
+    assert_equal "MMMM",Roman.convert(4000)
+    assert_equal "MMMMX",Roman.convert(4010)
+    assert_equal "CCCLXXXXVII",Roman.convert(397)
   end
 
 
