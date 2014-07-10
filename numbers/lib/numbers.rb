609b44755ce8f59e26548d9fb0e0ec46ca8beec1
Sujitha <sujitha@railsfactory.org>
2014-07-10 11:13:39 +0530

    numbers 1-31 conversion for date(st, th etc)

---
+++ numbers/lib/numbers.rb
@@ -0,0 +1,74 @@
+class Numbers
+
+  def self.convert(number)
+
+ if number == 1
+   s = 'first'
+  elsif number == 2
+    s ='second'
+  elsif number == 3
+     s ='third'
+ elsif number == 4
+    s = 'fourth'
+ elsif number == 5
+    s = 'fifth'
+  elsif number == 6
+    s = 'sixth'
+ elsif number == 7
+   s = 'seventh'
+  elsif number ==8
+   s = 'eighth'
+  elsif number == 9
+   s = 'nineth'
+  elsif number == 10
+   s = 'tenth'
+elsif number == 11
+   s = 'eleventh'
+  elsif number == 12
+    s ='twelveth'
+  elsif number == 13
+     s ='thirteenth'
+ elsif number == 14
+    s = 'fourteenth'
+ elsif number == 15
+    s = 'fifteenth'
+  elsif number == 16
+    s = 'sixteenth'
+ elsif number == 17
+   s = 'seventeenth'
+  elsif number == 18
+   s = 'eighteenth'
+  elsif number == 19
+   s = 'nineteenth'
+  elsif number == 20
+   s = 'twentyth'
+elsif number == 21
+   s = 'twentyfirst'
+  elsif number == 22
+    s ='twentysecond'
+  elsif number == 23
+     s ='twentythird'
+ elsif number == 24
+    s = 'twentyfourth'
+ elsif number == 25
+    s = 'twentyfifth'
+  elsif number == 26
+    s = 'twentysixth'
+ elsif number == 27
+   s = 'twentyseventh'
+  elsif number == 28
+   s = 'twentyeigth'
+  elsif number == 29
+   s = 'twentynineth'
+  elsif number == 30
+   s = 'thirtyth'
+ elsif number == 31
+  s = 'thirtyfirst'
+else number > 31
+  s= ''
+   
+  end
+  
+  end
+
+end 
