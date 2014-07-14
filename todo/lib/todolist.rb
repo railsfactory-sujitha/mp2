35720abb49feb31c89b223ef11f2dc9e3c21171b
Sujitha <sujitha@railsfactory.org>
2014-07-14 15:22:07 +0530

    Modified TODO task with all test cases

--- todo/lib/todolist.rb
+++ todo/lib/todolist.rb
@@ -1,63 +1,105 @@
 class Todolist
-attr_accessor :filename,:todo,:pending,:completed
-
 
 def initialize(filename)
-$filename = filename
-$todo = []
-$pending = []
-$completed = []
+ @filename = filename
+ @todo = []
+ @pending = []
+ @completed = []
 end
 
 def pending
-return $pending.size
+ @pending
 end
 
 def list
-$todo = $pending + $completed
-return $todo.size
+ @todo = @pending + @completed
+ @todo
 end
 
 def completed
-return $completed.size
+ @completed
 end
 
 def add(item)
-$pending << item
-return $pending.size
+  @pending << item
 end
 
 def complete(num)
-$completed << $pending[num - 1]
-$pending.delete_at(num - 1)
-return $completed.size
+   @completed << @pending[num - 1]
+     for i in 0...@completed.size
+       @completed[i].gsub!(/pending/,'complete')
+     end
+  @pending.delete_at(num - 1)
+   return @completed.size
 end
 
 def delete(num)
-$completed.delete_at(num-1)
-return $completed.size
+   @completed.delete_at(num-1)
+   return @completed.size
 end
 
 def modify(num , item)
-$pending[num-1] = item
-return $pending[num-1]
+    @pending[num-1] = item
+    return @pending[num-1]
 end
 
 def empty
-$pending.clear
-$completed.clear
-$todo.clear
-return true
+   @pending.clear
+   @completed.clear
+   @todo.clear
+  return true
 end
 
 def show_pending(num)
-return $pending[num-1]
+   return @pending[num-1]
 end
 
 def show_completed(num)
-return $completed[num-1]
+   return @completed[num-1]
+end
+
+def load_from_file
+
+  text=File.open(@filename).read
+  text.each_line do |line|
+  line.split('#')
+     if line.include?('pending')
+      @pending << line
+         else line.include?('complete')
+       @completed << line
+      end
+   end
+end
+
+
+
+def save(filename)
+  f = File.open(filename, "w")
+  str = ""
+  str = @todo.join("\n")
+  if (str == "complete" && str == "pending") then
+  return FAILURE
+  end
+  f.write(str)
+  f.close
+   count = 0
+   File.open(filename) {|f| count = f.read.count("")}
+   count+1
+end
+
+def save_to_file(filename)
+  f = File.open(filename, "w")
+  str = ""
+  str = @todo.join("\n")
+  if (str == "complete" && str == "pending") then
+  return FAILURE
+  end
+  f.write(str)
+  f.close
+   count = 0
+   File.open(filename) {|f| count = f.read.count("")}
+   count+1
+   File.delete(filename)
 end
 
 end
-
-
--- todo/test/test_todolist1.rb
+++ todo/test/test_todolist1.rb
@@ -1,101 +1,182 @@
 require_relative '../lib/todolist'
 require 'test/unit'
 
-class Test_todolist < Test::Unit::TestCase
 
-def setup
 
-@t = Todolist.new("file_suji.txt")
+class TestTodo < Test::Unit::TestCase
 
-end
+ def setup
 
-def teardown
-@t = nil
-end
+  @t = Todolist.new("todo.txt")
+ 
+ end
+
+ def teardown
+
+  @t=nil
+ 
+ end
 
 def test_empty
-
-@t.empty
-assert_equal 0, @t.pending.size
-assert_equal 0, @t.list.size
-assert_equal 0, @t.completed.size
+ 
+  @t.empty
+  assert_equal 0,@t.pending.size
+  assert_equal 0,@t.completed.size
+  assert_equal 0,@t.list.size
 end
 
-def test_add
+=begin
+ def test_add
+  @t.empty
+  @t.add("complete")
+  assert_equal 1,@t.pending.size
+  assert_equal 0,@t.completed.size
+  assert_equal 1,@t.list.size
+ end
 
-@t.add("item1")
-assert_equal 1, @t.pending.size
-assert_equal 1, @t.list.size
-assert_equal 0, @t.completed.size
-end
+def test_add2
+  @t.empty
+  @t.add("one")
+  @t.add("two")
+  assert_equal 2,@t.pending.size
+  assert_equal 0,@t.completed.size
+  assert_equal 2,@t.list.size
+ end
 
- def test_complete
-# precondition
-@t.empty
-@t.add("one")
-#before state
-assert_equal 1,@t.pending.size
-assert_equal 0,@t.completed.size
-assert_equal 1,@t.list.size
+def test_complete
+  @t.empty
+  @t.add("one")
+
+  assert_equal 1,@t.pending.size
+  assert_equal 0,@t.completed.size
+  assert_equal 1,@t.list.size
+
+  @t.complete(1)
+
+  assert_equal 0,@t.pending.size
+  assert_equal 1,@t.completed.size
+  assert_equal 1,@t.list.size
+ end
 
 
-#action
+
+  def test_delete
+   @t.empty
+   @t.add("one")
+   @t.complete(1)
+   
+   assert_equal 0,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 1,@t.list.size
+
+   @t.delete(1)
+
+   assert_equal 0,@t.pending.size
+   assert_equal 0,@t.completed.size
+   assert_equal 0,@t.list.size
+
+  end
+ 
+  
+
+ def test_modify
+   @t.empty
+   @t.add("one")
+   @t.add("two")
    @t.complete(1)
 
-#after 
-assert_equal 0,@t.pending.size
-assert_equal 1,@t.completed.size
-assert_equal 1,@t.list.size
-end
+   assert_equal 1,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+   @t.modify(1,"three")
+  
+   assert_equal 1,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+  end
+ 
+def test_pending
+   @t.empty
+   @t.add("one")
+   @t.add("two")
+  
 
-def test_delete
-#precondition
-@t.empty
-@t.add("item2")
-#before state
-assert_equal 1,@t.pending.size
-assert_equal 0,@t.completed.size
-assert_equal 1,@t.list.size
-#action
+   assert_equal 2,@t.pending.size
+   assert_equal 0,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+   @t.show_pending(2)
+     
+   assert_equal "two",@t.show_pending(2)
+   assert_equal 2,@t.pending.size
+   assert_equal 0,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+  end
+  
+def test_completed
+   @t.empty
+   @t.add("one")
+   @t.add("two")
    @t.complete(1)
-assert_equal 0,@t.pending.size
-assert_equal 1,@t.completed.size
-assert_equal 1,@t.list.size
- @t.delete(1)
-#after state
-assert_equal 0,@t.pending.size
-assert_equal 0,@t.completed.size
-assert_equal 0,@t.list.size
+
+   assert_equal 1,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+   @t.show_completed(1)
+     
+   assert_equal "one",@t.show_completed(1)
+   assert_equal 1,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 2,@t.list.size
+ 
+=end
+  
+def test_10_save
+   @t.empty
+
+   @t.add("item1#pending")
+   @t.add("item2#pending")
+   @t.add("item3#pending")
+  
+   
+   assert_equal 3,@t.pending.size
+   assert_equal 0,@t.completed.size
+   assert_equal 3,@t.list.size
+   
+   @t.save_to_file("a.txt")
+    
+   assert_equal 3,@t.pending.size
+   assert_equal 0,@t.completed.size
+   assert_equal 3,@t.list.size
 end
 
 
-def test_modify
-#precondition
-@t.empty
-@t.add("item3")
-#before state
-assert_equal 1,@t.pending.size
-assert_equal 0,@t.completed.size
-assert_equal 1,@t.list.size
-#action
-@t.modify(1,"item2_modify")
-#after state
-assert_equal 1,@t.pending.size
-assert_equal 0,@t.completed.size
-assert_equal 1,@t.list.size
-end
-
-
-def test_show_pending
-
-assert_equal "item2_modify", @t.show_pending(1)
-end
-
-def test_show_completed
-
-assert_equal nil, @t.show_completed(1)
+def test_load
+    #before action
+    @t.empty
+    assert_equal 0,@t.pending.size
+    assert_equal 0,@t.completed.size
+    assert_equal 0,@t.list.size
+    #action
+    @t.load_from_file
+    #after action
+    assert_equal 5,@t.pending.size
+    assert_equal 1,@t.completed.size
+    assert_equal 6,@t.list.size
+    #set 1 to be complete
+    @t.complete(1)
+    assert_equal 4,@t.pending.size
+    assert_equal 2,@t.completed.size
+    assert_equal 6,@t.list.size
+    @t.save("file_suji.txt")
+    assert_equal 4,@t.pending.size
+    assert_equal 2,@t.completed.size
+    assert_equal 6,@t.list.size
 end
 
 
 end
-
